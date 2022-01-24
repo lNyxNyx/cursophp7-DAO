289b4242d12b184de3d5b3298f254b8079d437d9
Sidnei Ferreira Jr <junior-sid99@hotmail.com>
2022-01-24 11:04:30 -0300

    Iniciando projeto

---
+++ class/sql.php
@@ -0,0 +1,32 @@
+<?php
+class Sql extends PDO
+{
+    private $conexao;
+
+    public function __construct()
+    {
+        $this->conexao = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+    }
+    private function setParams($stament, $parameters = array())
+    {
+        foreach ($parameters as $key => $value) {
+            $stament->bindParam($key, $value);
+        }
+    }
+    private function setParan($stament, $key, $value)
+    {
+        $stament->bindParam($key, $value);
+    }
+    public function query($rawQuery, $params = array())
+    {
+        $stmt = $this->conexao->prepare($rawQuery);
+        $this->setParams($stmt, $params);
+        $stmt->execute();
+        return $stmt;
+    }
+    public function select($rawQuery, $params = array()): array
+    {
+        $stmt = $this->query($rawQuery, $params);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
